092445a0c306ebcaa868c5c0a6d745bdd72f078f
Ioan Vapirovschi <vapirovschi@optymyze.com>
2015-12-30 12:41:42 +0200

    try to use poweshell get-process instead of tasklist

--- app.go
+++ app.go
@@ -6,7 +6,7 @@ import (
     "strings"
     "bufio"
     "log"
-    "syscall"
+    "strconv"
 )
 
 const (
@@ -18,13 +18,68 @@ const (
 func minecraftPIDs() ([]string, error) {
     var pids []string
 
-    out, err := exec.Command("TASKLIST", "-V", "-FI", "\"javaw.exe\"").Output()
+    out, err := exec.Command("powershell", "get-process javaw | select-object id,mainwindowtitle").Output()
+    if err != nil {
+        return pids, err
+    }
+
+    log.Printf("Tasklist output: '%s'\n", string(out))
+
+    scanner := bufio.NewScanner(strings.NewReader(string(out)))
+
+    // scan output line by line
+    for scanner.Scan() {
+        // split a line in tokens
+        line := scanner.Text()
+        tokens := strings.Fields(line)
+        // there must be at least 2 tokens (PID is the 1st)
+        if len(tokens) < 1 {
+            continue
+        }
+
+        // running as service we don't get the 'window title' as running from cmd
+        // and we cannot select minecraft app only. We have to kill all javaw
+        token := strings.TrimSpace(tokens[0])
+        _, err = strconv.Atoi(token)
+        if err == nil {
+            pids = append(pids, tokens[0])
+        }
+/*
+        // search for marker and get the PID
+        for _, token := range tokens {
+            if strings.HasPrefix(strings.ToLower(token), markerToken) {
+                pids = append(pids, tokens[0])
+                log.Println(line)
+            }
+        }
+*/
+    }
+
+    if len(pids) == 0 {
+        return pids, fmt.Errorf("Cannot find any Minecraft process.")
+    }
+
+    log.Println(pids)
+    return pids, nil
+}
+
+func minecraftPIDs3() ([]string, error) {
+    var pids []string
+
+    cmd := "TASKLIST"
+    args := []string{
+        "/V",
+        "/FI",
+        fmt.Sprintf("\"IMAGENAME eq %s\"", appToken),
+    }
+
+    out, err := exec.Command(cmd, args...).Output()
 //    out, err := exec.Command("cmd", "/C", "TASKLIST", "/V").Output()
     if err != nil {
         return pids, err
     }
 
-    log.Println(string(out))
+    log.Printf("Tasklist output: '%s'\n", string(out))
 
     scanner := bufio.NewScanner(strings.NewReader(string(out)))
 
