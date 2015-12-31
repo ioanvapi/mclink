f3220aa8e70cebc9bc862c63f18d88bb9f465139
Ioan Vapirovschi <vapirovschi@optymyze.com>
2015-12-31 10:22:25 +0200

    try to use poweshell get-process instead of tasklist

--- app.go
+++ app.go
@@ -18,6 +18,7 @@ const (
 func minecraftPIDs() ([]string, error) {
     var pids []string
 
+    // get pid and main window title for every javax process
     out, err := exec.Command("powershell", "get-process javaw | select-object id,mainwindowtitle").Output()
     if err != nil {
         return pids, err
@@ -53,53 +54,6 @@ func minecraftPIDs() ([]string, error) {
             }
         }
 */
-    }
-
-    if len(pids) == 0 {
-        return pids, fmt.Errorf("Cannot find any Minecraft process.")
-    }
-
-    log.Println(pids)
-    return pids, nil
-}
-
-func minecraftPIDs3() ([]string, error) {
-    var pids []string
-
-    cmd := "TASKLIST"
-    args := []string{
-        "/V",
-        "/FI",
-        fmt.Sprintf("\"IMAGENAME eq %s\"", appToken),
-    }
-
-    out, err := exec.Command(cmd, args...).Output()
-//    out, err := exec.Command("cmd", "/C", "TASKLIST", "/V").Output()
-    if err != nil {
-        return pids, err
-    }
-
-    log.Printf("Tasklist output: '%s'\n", string(out))
-
-    scanner := bufio.NewScanner(strings.NewReader(string(out)))
-
-    // scan output line by line
-    for scanner.Scan() {
-        // split a line in tokens
-        line := scanner.Text()
-        tokens := strings.Fields(line)
-        // there must be at least 2 tokens (PID is the second) and first should be appToken
-        if len(tokens) < 2 || !strings.HasPrefix(tokens[0], appToken) {
-            continue
-        }
-
-        // search for marker and get the PID
-        for _, token := range tokens {
-            if strings.HasPrefix(strings.ToLower(token), markerToken) {
-                pids = append(pids, tokens[1])
-                log.Println(line)
-            }
-        }
     }
 
     if len(pids) == 0 {
